29a751bef02ba25a86265446f1055365eb16f773
dmitry.grunt <dmitry.grunt@protonmail.com>
2022-09-22 09:09:00 +0300

    Slick config test

--- components/ui/ProductCarousel.js
+++ components/ui/ProductCarousel.js
@@ -19,13 +19,49 @@ const ProductCarousel = () => {
     getData();
   }, []);
 
+  // const config = {
+  //   infinite: true,
+  //   prevArrow: <PrevArrow />,
+  //   nextArrow: <NextArrow />,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  // };
+
   const config = {
-    infinite: true,
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
+    dots: false,
+    infinite: false,
+    speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
+
 
   const [settings, setSettings] = useState(config);
 
