1c01879ce008cc1c5343f6c888324a6c6cce5ed5
dmitry.grunt <dmitry.grunt@protonmail.com>
2022-09-22 09:09:13 +0300

    Revert "Slick config test"
    This reverts commit 29a751bef02ba25a86265446f1055365eb16f773.

--- components/ui/ProductCarousel.js
+++ components/ui/ProductCarousel.js
@@ -19,49 +19,13 @@ const ProductCarousel = () => {
     getData();
   }, []);
 
-  // const config = {
-  //   infinite: true,
-  //   prevArrow: <PrevArrow />,
-  //   nextArrow: <NextArrow />,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  // };
-
   const config = {
-    dots: false,
-    infinite: false,
-    speed: 500,
+    infinite: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
     slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      }
-    ]
+    slidesToScroll: 1,
   };
-
 
   const [settings, setSettings] = useState(config);
 
