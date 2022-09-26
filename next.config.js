b4f3715cf28704b98fe0effa29eff3d6923f4b26
dmitry.grunt <dmitry.grunt@protonmail.com>
2022-09-26 12:25:20 +0300

    docker

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  distDir: 'build',
+  }
 };
 
